6be4139534cf9c63cb058a49f9ed49761b48559f
miguelcastaneda1765-lab <miguelcastaneda1765@gmail.com>
2026-04-29 17:30:23 +0200

    Menu

--- scripts/main_menu/Menu.cs
+++ scripts/main_menu/Menu.cs
@@ -5,72 +5,72 @@ using System.Net.Sockets;
 
 namespace main
 {
-    public partial class Menu : Node
-    {
-        private LineEdit _ipInput;
-        private PlayersManager _playersManager;
+	public partial class Menu : Node
+	{
+		private LineEdit _ipInput;
+		private PlayersManager _playersManager;
 
-        public override void _Ready()
-        {
-            _playersManager = GetNode<PlayersManager>("/root/PlayersManager");
+		public override void _Ready()
+		{
+			_playersManager = GetNode<PlayersManager>("/root/PlayersManager");
 
-            Button hostBtn = GetNode<Button>("HostBtn");
-            Button joinBtn = GetNode<Button>("JoinBtn");
+			Button hostBtn = GetNode<Button>("MenuContainer/HostBtn");
+			Button joinBtn = GetNode<Button>("MenuContainer/JoinBtn");
 
-            Label hostingIP = new Label();
-            hostingIP.Text = $"Your IP: {GetLocalIPAddress()}";
-            AddChild(hostingIP);
+			Label hostingIP = new Label();
+			hostingIP.Text = $"Your IP: {GetLocalIPAddress()}";
+			AddChild(hostingIP);
 
-            _ipInput = GetNode<LineEdit>("HostIPInput");
-            _ipInput.PlaceholderText = "Enter host IP to join";
-            _ipInput.Text = "";
+			_ipInput = GetNode<LineEdit>("MenuContainer/HostIPInput");
+			_ipInput.PlaceholderText = "Enter host IP to join";
+			_ipInput.Text = "";
 
-            hostBtn.Pressed += OnHostPressed;
-            joinBtn.Pressed += OnJoinPressed;
-        }
+			hostBtn.Pressed += OnHostPressed;
+			joinBtn.Pressed += OnJoinPressed;
+		}
 
-        private void OnHostPressed()
-        {
-            GameState.Role = GameState.NetworkRole.Server;
-            GameState.HostIP = GetLocalIPAddress();
-            _playersManager.StartServer();
-            GetTree().ChangeSceneToFile("res://players_pool.tscn");
-        }
+		private void OnHostPressed()
+		{
+			GameState.Role = GameState.NetworkRole.Server;
+			GameState.HostIP = GetLocalIPAddress();
+			_playersManager.StartServer();
+			GetTree().ChangeSceneToFile("res://players_pool.tscn");
+		}
 
-        private void OnJoinPressed()
-        {
-            GameState.Role = GameState.NetworkRole.Client;
-            GameState.HostIP = _ipInput.Text;
-            _playersManager.StartClient();
-            GetTree().ChangeSceneToFile("res://players_pool.tscn");
-        }
+		private void OnJoinPressed()
+		{
+			GameState.Role = GameState.NetworkRole.Client;
+			GameState.HostIP = _ipInput.Text;
+			_playersManager.StartClient();
+			GetTree().ChangeSceneToFile("res://players_pool.tscn");
+		}
 
-        private static string GetLocalIPAddress()
-        {
-            try
-            {
-                foreach (var iface in System.Net.NetworkInformation.NetworkInterface.GetAllNetworkInterfaces())
-                {
-                    if (iface.OperationalStatus != System.Net.NetworkInformation.OperationalStatus.Up) continue;
-                    foreach (var addr in iface.GetIPProperties().UnicastAddresses)
-                    {
-                        if (addr.Address.AddressFamily == AddressFamily.InterNetwork &&
-                            !System.Net.IPAddress.IsLoopback(addr.Address))
-                            return addr.Address.ToString();
-                    }
-                }
-            }
-            catch { }
+		private static string GetLocalIPAddress()
+		{
+			try
+			{
+				foreach (var iface in System.Net.NetworkInformation.NetworkInterface.GetAllNetworkInterfaces())
+				{
+					if (iface.OperationalStatus != System.Net.NetworkInformation.OperationalStatus.Up) continue;
+					foreach (var addr in iface.GetIPProperties().UnicastAddresses)
+					{
+						if (addr.Address.AddressFamily == AddressFamily.InterNetwork &&
+							!System.Net.IPAddress.IsLoopback(addr.Address))
+							return addr.Address.ToString();
+					}
+				}
+			}
+			catch { }
 
-            try
-            {
-                foreach (var ip in Dns.GetHostEntry(Dns.GetHostName()).AddressList)
-                    if (ip.AddressFamily == AddressFamily.InterNetwork)
-                        return ip.ToString();
-            }
-            catch { }
+			try
+			{
+				foreach (var ip in Dns.GetHostEntry(Dns.GetHostName()).AddressList)
+					if (ip.AddressFamily == AddressFamily.InterNetwork)
+						return ip.ToString();
+			}
+			catch { }
 
-            return "127.0.0.1";
-        }
-    }
+			return "127.0.0.1";
+		}
+	}
 }
